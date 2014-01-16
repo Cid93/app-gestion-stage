b55da5fd568b2f1dfef356bb5f5e7e341e522e43
Yacine <yacine.aitmaamar@gmail.com>
2014-01-16 01:39:03 +0100

    Ajouts et implementation des fichiers admin.py et forms.py (formulaire d ajout et de supression de stage)

---
+++ stage/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from stage.models import Diplome,Promotion,Personne,Etudiant,Enseignant,Stage,logiciel,Stage_logiciel,EnseignantResp
+
+
+admin.site.register(Diplome)
+admin.site.register(Promotion)
+admin.site.register(Personne)
+admin.site.register(Etudiant)
+admin.site.register(Enseignant)
+admin.site.register(Stage)
+admin.site.register(logiciel)
+admin.site.register(Stage_logiciel)
+admin.site.register(EnseignantResp)
---
+++ stage/forms.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from django.forms import ModelForm
+from django import forms
+from stage.models import Stage
+
+class StageForm(ModelForm):
+    class Meta:
+        model = Stage
+
+class supprimeStageForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(supprimeStageForm, self).__init__(*args, **kwargs)
+        self.fields['stageslots'].label = "selectionnez les stages à supprimer"
+    # You can change the queryset in the __init__ method, but this should be a nice basis
+        
+    stageslots = forms.ModelMultipleChoiceField(queryset=Stage.objects.all(), widget=forms.CheckboxSelectMultiple(),required=False)
+
+
+    def save(self):
+        # make sure you do a form.is_valid() before trying to save()
+        for stageslot in self.cleaned_data['stageslots']:
+            stageslot.delete()
+    def list(self):
+        # make sure you do a form.is_valid() before trying to save()
+        for stageslot in self.cleaned_data['stageslots']:
+            print(stageslot)
+    class Meta:
+        model = Stage
+        fields ='stageslots'
