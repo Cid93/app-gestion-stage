e9b2189495a5dcebbaaea088c9d41d3333659d69
Yacine <c.outreville@gmx.fr>
2014-01-16 01:57:07 +0100

    modification du fichier views.py ajout des vues, show_detail_stage, addStage

--- stage/views.py
+++ stage/views.py
@@ -1,9 +1,40 @@
+# -*- coding: utf-8 -*-
 # Create your views here.
+from stage.models import Stage
+from stage.forms import StageForm, supprimeStageForm
 from django.shortcuts import render_to_response, HttpResponseRedirect, HttpResponse
 from django.forms import ModelForm
 from django.template import RequestContext
+from django.core.context_processors import csrf
+#from stage.forms import supprimeStageForm
 
 def show_stages(request):
 	return render_to_response(
-		"stages/stage.html",
-		{})
+		"stage/stage.html",
+		{"liste_stage": Stage.objects.order_by("intitule")})
+
+def show_detail_stage(request, pk):
+	return render_to_response(
+		"stage/detail_stage.html",
+		{"stage": Stage.objects.get(pk=pk)}
+		)
+
+# Manipulation Entreprise
+def addStage(request):
+	#entreprise_form = EntrepriseForm()
+	#form = EntrepriseForm(instance=Entreprise.objects.all()[1])
+
+	if request.method == 'POST':  # S'il s'agit d'une requête POST
+		form = StageForm(request.POST)  # Nous reprenons les données
+
+		if form.is_valid(): # Nous vérifions que les données envoyées sont valides
+			form.save()
+			return HttpResponseRedirect('/stage')
+
+	form = StageForm()  # Nous créons un formulaire vide
+	con = { 'actionAFaire' : 'Ajouter', 'form' : form}
+
+	return render_to_response('stage/forms.html',
+							con,
+							context_instance=RequestContext(request))
+	#return render(request, 'addEnt.html', locals())
