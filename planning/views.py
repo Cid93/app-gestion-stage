4466f38d49bd6f1c822ac436b19f602df0add4e4
Cedric Outreville <outreville@info22-15.iut45.univ-orleans.fr>
2014-03-31 11:54:46 +0200

    mise en place de l'affichage du formulaire erroné si mal renseigné

--- planning/views.py
+++ planning/views.py
@@ -38,19 +38,19 @@ def addSoutenance(request):
 			
 			if form.is_valid(): 					# Si les données reçues sont valides
 				form.save()
-				# try:
-				request.POST['ajax']
-				return HttpResponse(
-					serializers.serialize(
-						"json",
-						Soutenance.objects.filter(stage=
-							Stage.objects.get(pk=request.POST['stage'])),
-						indent = 2, 
-						use_natural_keys=True
-					),
-					mimetype="application/json")
-				# except:
-				# 	return HttpResponseRedirect('/planning/')
+				try:
+					request.POST['ajax']
+					return HttpResponse(
+						serializers.serialize(
+							"json",
+							Soutenance.objects.filter(stage=
+								Stage.objects.get(pk=request.POST['stage'])),
+							indent = 2, 
+							use_natural_keys=True
+						),
+						mimetype="application/json")
+				except:
+					return HttpResponseRedirect('/planning/')
 			else:									# Si les données reçues sont invalides
 				con = { 'actionAFaire' : 'Ajouter', 'form' : form}
 				return render(request,'planning/forms.html', con)
