bdbd009405d36e02b705766b474a4b9de8fcd394
Cedric Outreville <outreville@info22-15.iut45.univ-orleans.fr>
2014-04-02 11:48:04 +0200

    restriction des stages afficher lors de la création d'une soutenance (seulement si stage validé)

--- planning/forms.py
+++ planning/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 
 from planning.models import Soutenance, Salle
+from stage.models import Stage
 
 class SoutenanceSelection(ModelForm):
     class Meta:
@@ -9,8 +10,10 @@ class SoutenanceSelection(ModelForm):
         exclude = ['idSoutenance', 'stage', 'salle']
 
 class SoutenanceForm(ModelForm):
-	class Meta:
-		model = Soutenance
+    stage = forms.ModelChoiceField(queryset=Stage.objects.filter(valideStage=True))
+
+    class Meta:
+        model = Soutenance
 
 class SalleForm(ModelForm):
 	class Meta:
@@ -23,9 +26,9 @@ class supprimerSoutenanceForm(forms.Form):
     # You can change the queryset in the __init__ method, but this should be a nice basis
         
     soutenanceslots = forms.ModelMultipleChoiceField(
-    	queryset=Soutenance.objects.all(),
-    	widget=forms.CheckboxSelectMultiple(),
-    	required=False)
+        queryset=Soutenance.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=False)
 
     def save(self):
         # make sure you do a form.is_valid() before trying to save()
